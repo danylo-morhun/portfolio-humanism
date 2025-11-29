fba5a78243d40553d3973d82ebae84a23ef3c811
Danylo Morhun <danymorhun@gmail.com>
2025-11-29 20:27:37 +0100

    feat: add vue router configuration

---
+++ src/router/index.ts
@@ -0,0 +1,47 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import AboutView from '../views/AboutView.vue';
+import ProjectsView from '../views/ProjectsView.vue';
+import ProjectDetailView from '../views/ProjectDetailView.vue';
+import ContactView from '../views/ContactView.vue';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView,
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      component: ProjectsView,
+    },
+    {
+      path: '/projects/:id',
+      name: 'project-detail',
+      component: ProjectDetailView,
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: ContactView,
+    },
+  ],
+  scrollBehavior(_to, _from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  },
+});
+
+export default router;
+
