c1461d85774764ee2fccbabfaf68cd4754dc7dc8
Danylo Morhun <danymorhun@gmail.com>
2025-11-29 20:27:49 +0100

    feat: add home view

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import HeroSection from '../components/HeroSection.vue';
+import SelectedWorkSection from '../components/SelectedWorkSection.vue';
+import ExperienceSection from '../components/ExperienceSection.vue';
+import PlaygroundSection from '../components/PlaygroundSection.vue';
+import SideNavigation from '../components/SideNavigation.vue';
+import { useScrollAnimation } from '../composables/useScrollAnimation';
+
+useScrollAnimation();
+</script>
+
+<template>
+  <div>
+    <SideNavigation />
+    <HeroSection />
+    <SelectedWorkSection />
+    <ExperienceSection />
+    <PlaygroundSection />
+  </div>
+</template>
+
